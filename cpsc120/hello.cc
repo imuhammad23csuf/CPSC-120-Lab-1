d6d72233b03fc8538faddbf797d0cf4722490677
Muhammad <imuhammad23@cs101u-2c.acad.fullerton.edu>
2023-08-30 19:05:35 -0700

    Added a personalized header. My first git commit.

---
+++ cpsc120/hello.cc
@@ -0,0 +1,11 @@
+// Ibraheem Muhammad
+// imuhammad23@csu.fullerton.edu
+// @imuhammad23csuf
+// Partners: @Hooriya-Muh
+
+#include <iostream>
+
+int main(void){
+    cout << "Hello World!\n";
+    return 0;
+}
